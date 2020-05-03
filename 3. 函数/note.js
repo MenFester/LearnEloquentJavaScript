1e1b4b35901b5fbad1e518a03cf2c12bb41f3ad3
MenFester <13500177552@qq.com>
2020-05-03 23:33:27 +0800

    2020年05月03日23:33

--- 3. 函数/note.js
+++ 3. 函数/note.js
@@ -1,4 +1,4 @@
-const square = function(x) {
+const square = function(x) {    // 绑定表示法
     return x * x
 };
 console.log(square(12));
@@ -9,7 +9,7 @@ const power = function(base, exponent) {
         result *= base;
     }
     return result
-}
+};
 console.log(power(2, 10))
 
 let x = 10;
@@ -21,6 +21,50 @@ if (true) {
 }
 const tmpFunc = function() {
     var r = 0
-}
+};
 // 此处y不可见
 console.log(x + z);    // 此处z可见、r不可见，因为z不是定义在函数中
+
+function squareFunc(x) {    // 声明表示法
+    return x * x;
+}
+
+const power2 = (base, exponent) => {    // 箭头函数绑定
+    let result = 1;
+    for (let count = 0; count < exponent; count++) {
+        result = result * base;
+    }
+    
+    return result;
+};
+
+const square2 = x => x * x;    // 等同于 const square2 = (x) => { return x * x; };
+console.log(square2(10));
+
+function power3(base, exponent=2) {
+    let result = 1;
+    for (let count = 0; count < exponent; count++) {
+        result = result * base;
+    }
+    
+    return result;
+}
+
+console.log(power3(4));
+console.log(power3(2, 6));
+
+function multiplier(factor) {    // 创建闭包
+    return number => number * factor;
+}
+
+let twice = multiplier(2);
+console.log(twice(5));
+
+function power4(base, exponent) {    // power函数的递归实现
+    if (exponent == 0) {
+        return 1;
+    } else {
+        return base * power4(base, exponent - 1);
+    }
+}
+console.log(power4(2, 3));
--- test.js
+++
@@ -1,3 +0,0 @@
-s = ` 1 + 1 = ${ 1 + 1} `;
-console.log(s);
-console.log("Hello, World!");
