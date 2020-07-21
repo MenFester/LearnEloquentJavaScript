229b4fa7028ffa99de05f7e65811fb7f62d83a1d
MenFester <13500177552@qq.com>
2020-07-21 10:49:02 +0800

    2020年07月21日 10:48

--- 9. 正则表达式/note.js
+++ 9. 正则表达式/note.js
@@ -28,3 +28,33 @@ console.log(/^\d+/.test("abcdefghijklmn1234"));    // false
 console.log(/^\d+/.test("12345678"));    // true
 console.log(/^\d+/.test("m12345678"));    // false
 console.log(/[^\d]/.test("abcdefghijklmn"));    // true
+
+console.log(/neighbou?r/.test("neighbor"));    // true
+
+let dateTime = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{2}/;
+console.log(dateTime.test("1-30-2003 8:45"));    // true
+
+console.log(/boo+(hoo+)+/i.test("Boohoooohoohooo"));    //true
+
+let match = /\d+/.exec("one two 100");
+console.log(match);    // [ '100', index: 8, input: 'one two 100', groups: undefined ]
+console.log(match.index);    // 8
+console.log("one two 100".match(/\d+/));
+let match2 = /\d+/.exec("one two 100 200 300");
+console.log(match2);
+
+let quotedText = /'([^']*)'/;
+console.log(quotedText.exec("she said 'hello'"));
+
+console.log(/bad(ly)?/.exec("bad"));    // [ 'bad', undefined, index: 0, input: 'bad', groups: undefined ]
+console.log(/(\d)+/.exec("123"));    // [ '123', '3', index: 0, input: '123', groups: undefined ]
+
+console.log(new Date());
+console.log(new Date(2009, 11, 9));
+console.log(new Date(2013, 11, 19).getTime());
+
+function getDate(string) {
+    let [_, month, day, year] = /(\d{1,2})-(\d{1,2})-(\d{4})/.exec(string);
+    return new Date(year, month-1, day);
+}
+console.log(getDate("1-30-2003"));
