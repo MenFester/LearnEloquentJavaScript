09407a1656301d30af37e769566b43c76209d32d
MenFester <13500177552@qq.com>
2020-07-30 17:48:59 +0800

    2020年07月30日 17:48

---
+++ 10. 模块/note.js
@@ -0,0 +1,20 @@
+const weekDay = function() {
+    const names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    return {
+        name(number) { return names[number]; },
+        number(name) { return names.indexOf(name); }
+    };
+}();
+console.log(weekDay.name(weekDay.number("Sunday")));
+
+const x = 1;
+function evalAndReturnX(code) {
+    eval(code);
+    return x;
+}
+console.log(evalAndReturnX("var x = 2"));
+console.log(x);
+
+const {formatDate} = require("./format-date");
+console.log(formatDate(new Date(2019, 8, 13), "dddd the Do"));
