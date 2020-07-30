df78e6375421c14289397c34d4b2fa4676bf74e5
MenFester <13500177552@qq.com>
2020-07-30 18:21:38 +0800

    2020年07月30日 18:21

--- 10. 模块/note.js
+++ 10. 模块/note.js
@@ -18,3 +18,17 @@ console.log(x);
 
 const {formatDate} = require("./format-date");
 console.log(formatDate(new Date(2019, 8, 13), "dddd the Do"));
+
+// 最小形式定义require，解析require的原理
+
+require.cache = Object.create(null);
+function require(name) {
+    if (!(name in require.cache)) {
+        let code = readFile(name);
+        let module = { exports: {} };
+        require.cache[name] = module;
+        let wrapper = Function("require, exports, module", code);
+        wrapper(require, module.exports, module);
+    }
+    return require.cache[name].exports;
+}
