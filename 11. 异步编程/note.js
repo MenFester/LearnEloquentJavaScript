e3a55efdcf0fb7433559d6c31210f53b6fb86db6
MenFester <13500177552@qq.com>
2020-08-03 10:31:46 +0800

    2020年08月02日 10:31

--- 11. 异步编程/note.js
+++ 11. 异步编程/note.js
@@ -17,3 +17,18 @@ new Promise(resolve => {
 }).then(val => {
     console.log(val);
 });
+
+new Promise((_, reject) => reject(new Error("Fail")))
+.then(value => console.log("Handler 1"))
+.catch(reason => {
+    console.log("Caught failure " + reason);    // Caught failure Error: Fail
+    let fail_flag = 0;
+    if (fail_flag) {
+        return new Promise((_, reject) => reject(new Error("Fail again.")));    // 模拟再次失败处理
+    } else {
+        return "nothing";    // 模拟成功处理，返回nothing结果
+    }  
+    
+})
+.then(value => console.log("Handler 2", value))    // Handler 2 nothing
+.catch(reason => console.log("Caught failure " + reason));    // Caught failure Error: Fail again.
